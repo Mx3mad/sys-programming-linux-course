aca295147268e9b4ac18100b59620d31acd8807a
Mx3mad <muhammad.emad1233@gmail.com>
2025-04-21 13:56:24 +0200

    Fixed the indentation

--- pico_shell.c
+++ pico_shell.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
 	int exit_code = 0;
-    if (argc > 1)
+	if (argc > 1)
 	{
 		printf("Usage: Pico Shell doesn't have arguments, please run it again without arguments\n");
 		exit(-1);
@@ -21,24 +21,24 @@ int main(int argc, char **argv)
 	{
 		input[0] = 0;
 		if (feof(stdin))
-		    break;
+			break;
 		printf("PicoSha >> ");
 		fflush(stdout);
 		if (fgets(input, IN_SIZE, stdin) == NULL)
 		{
 			//printf("EOF or Failed to read the input\n");
-		    break;
+			break;
 		}
 		input[strlen(input) - 1] = 0;
 		if (strlen(input) == 0)
 			continue;
 		int new_argc = strlen(input);
-		char **new_argv = (char**)malloc(sizeof(char*) * new_argc + 1);
+		char **new_argv = (char **)malloc(sizeof(char *) * new_argc + 1);
 		char *temp = strtok(input, " ");
 		int i = 1, k = 0;
 		while (temp != NULL)
 		{
-			new_argv[k] = (char*)malloc(strlen(temp) + 1);
+			new_argv[k] = (char *)malloc(strlen(temp) + 1);
 			strcpy(new_argv[k], temp);
 			k++;
 			temp = strtok(NULL, " ");
@@ -67,8 +67,8 @@ int main(int argc, char **argv)
 			}
 			else
 			{
-			    printf("%s\n", cwd);
-			    exit_code = 0;
+				printf("%s\n", cwd);
+				exit_code = 0;
 			}
 		}
 		else if (!strcmp(new_argv[0], "cd"))
@@ -81,31 +81,31 @@ int main(int argc, char **argv)
 				exit_code = 1;
 			}
 			else
-			    exit_code = 0;
+				exit_code = 0;
 		}
 		else if (!strcmp(new_argv[0], "exit"))
 		{
 			if (new_argc > 1)
 				printf("Usage: 'exit' command doesn't have arguments. Arguments are ignored.\n");
 			printf("Good Bye\n");
-			for(i = 0; i < new_argc; i++)
-    			free(new_argv[i]);
-    		free(new_argv);
+			for (i = 0; i < new_argc; i++)
+				free(new_argv[i]);
+			free(new_argv);
 			break;
 		}
 		else
 		{
 			pid_t child_pid = fork();
-			if (child_pid > 0) //This is parent
+			if (child_pid > 0)	//This is parent
 			{
 				int status;
 				wait(&status);
 				if (WIFEXITED(status))
-				    exit_code = WEXITSTATUS(status);
+					exit_code = WEXITSTATUS(status);
 				else
-				    exit_code = 1;
+					exit_code = 1;
 			}
-			else if (child_pid == 0) //This is child
+			else if (child_pid == 0)	//This is child
 			{
 				execvp(new_argv[0], new_argv);
 				printf("%s: command not found\n", new_argv[0]);
@@ -114,7 +114,7 @@ int main(int argc, char **argv)
 			else
 				printf("UNIX CMD: Failed to fork\n");
 		}
-		for(i = 0; i < new_argc; i++)
+		for (i = 0; i < new_argc; i++)
 			free(new_argv[i]);
 		free(new_argv);
 	}
